3723e86fd93a53e48667d1a3ca9d468b94c1dc64
Arthur Aguiar Silva <arthuraguiarsilva1@gmail.com>
2026-05-27 20:00:05 -0300

    atualização3 commit

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbKFdJwA-Hz6QIi_D04kB_HCUF4LY",
+  apiKey: "SUA_KEY",
   authDomain: "campeonato-bairro-f1b14.firebaseapp.com",
   databaseURL: "https://campeonato-bairro-f1b14-default-rtdb.firebaseio.com",
   projectId: "campeonato-bairro-f1b14",
   storageBucket: "campeonato-bairro-f1b14.firebasestorage.app",
   messagingSenderId: "107353619301",
   appId: "1:107353619301:web:ae1febb3a5068248167fc",
-  measurementId: "G-7SJKYVL947",
 };
 
 const app = initializeApp(firebaseConfig);
